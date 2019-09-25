d9914eced8d5425f47a9e065efae54b1a3a35580
Christine <christine-xia@hotmail.com>
2019-09-25 19:55:48 -0400

    Moved pages to index

---
+++ pages/Web_Typographic_Poster/js/script.js
@@ -0,0 +1,95 @@
+
+///// HOVER SIDEBAR
+
+$(".shared-area-1").hover(function(){
+	$(".shared-area-1, .shared-area-1a").addClass("hovering")
+}, function(){
+	$(".shared-area-1, .shared-area-1a").removeClass("hovering")	
+})
+
+$(".shared-area-2").hover(function(){
+	$(".shared-area-2, .shared-area-2a").addClass("hovering")
+}, function(){
+	$(".shared-area-2, .shared-area-2a").removeClass("hovering")	
+})
+
+$(".shared-area-3").hover(function(){
+	$(".shared-area-3, .shared-area-3a").addClass("hovering")
+}, function(){
+	$(".shared-area-3, .shared-area-3a").removeClass("hovering")	
+})
+
+$(".shared-area-4").hover(function(){
+	$(".shared-area-4, .shared-area-4a").addClass("hovering")
+}, function(){
+	$(".shared-area-4, .shared-area-4a").removeClass("hovering")	
+})
+
+$(".shared-area-5").hover(function(){
+	$(".shared-area-5, .shared-area-5a").addClass("hovering")
+}, function(){
+	$(".shared-area-5, .shared-area-5a").removeClass("hovering")	
+})
+
+$(".shared-area-6").hover(function(){
+	$(".shared-area-6, .shared-area-6a").addClass("hovering")
+}, function(){
+	$(".shared-area-6, .shared-area-6a").removeClass("hovering")	
+})
+
+$(".shared-area-7").hover(function(){
+	$(".shared-area-7, .shared-area-7a").addClass("hovering")
+}, function(){
+	$(".shared-area-7, .shared-area-7a").removeClass("hovering")	
+})
+
+
+///// HOVER POST
+
+$(".shared-area-1a").hover(function(){
+	$(".shared-area-1, .shared-area-1a").addClass("hovering")
+}, function(){
+	$(".shared-area-1, .shared-area-1a").removeClass("hovering")	
+})
+
+$(".shared-area-2a").hover(function(){
+	$(".shared-area-2, .shared-area-2a").addClass("hovering")
+}, function(){
+	$(".shared-area-2, .shared-area-2a").removeClass("hovering")	
+})
+
+$(".shared-area-3a").hover(function(){
+	$(".shared-area-3, .shared-area-3a").addClass("hovering")
+}, function(){
+	$(".shared-area-3, .shared-area-3a").removeClass("hovering")	
+})
+
+$(".shared-area-4a").hover(function(){
+	$(".shared-area-4, .shared-area-4a").addClass("hovering")
+}, function(){
+	$(".shared-area-4, .shared-area-4a").removeClass("hovering")	
+})
+
+$(".shared-area-5a").hover(function(){
+	$(".shared-area-5, .shared-area-5a").addClass("hovering")
+}, function(){
+	$(".shared-area-5, .shared-area-5a").removeClass("hovering")	
+})
+
+$(".shared-area-6a").hover(function(){
+	$(".shared-area-6, .shared-area-6a").addClass("hovering")
+}, function(){
+	$(".shared-area-6, .shared-area-6a").removeClass("hovering")	
+})
+
+$(".shared-area-7a").hover(function(){
+	$(".shared-area-7, .shared-area-7a").addClass("hovering")
+}, function(){
+	$(".shared-area-7, .shared-area-7a").removeClass("hovering")	
+})
+
+
+
+
+
+
