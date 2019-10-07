b082e8f34b465947631e47b90cbf69d56253e4f9
Christine <christine-xia@hotmail.com>
2019-10-07 17:53:01 -0400

    Javascript tests

---
+++ in-class-stuff/first-script/js/first-script.js
@@ -0,0 +1,13 @@
+console.log("hello world");
+
+alert("AAAAHHHH");
+
+
+var i;
+
+for (i = 0; i < 255; i++) {
+	var newElement = $('<div class="element" style="color:rgb(' + i + ',' + (255 - i) + ',255"></div>').text('I am element #' + (i + 1));
+	$("#first-element").append(newElement);
+}
+
+$(".element").css({"width":"100%", "font-size":"18pt", "text-align":"center"})
