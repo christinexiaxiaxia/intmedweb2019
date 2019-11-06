a56b00990f3c595b85e4cb59d8302def50fb04c0
Christine <christine-xia@hotmail.com>
2019-11-06 04:16:48 -0500

    minor changes

--- act-3/ideas/js/main.js
+++ act-3/ideas/js/main.js
@@ -1,12 +1,4 @@
 
-// for (var i = 0; i < 250; i++) {
-// 	var leftText = $('<p>I am on the left</p>');
-// 	var rightText = $('<p>I am on the right</p>');
-// 	console.log('Appended');
-
-// 	$('.left').append(leftText);
-// 	$('.right').append(rightText);
-// }
 
 ///// HOVER LINK
 
@@ -15,7 +7,3 @@ $('a').hover(function(){
 }, function(){
 	$('.link-ind').css({'opacity':1})	
 })
-
-// var linkIndicator = $('.link-ind').css({'opacity':'0'});
-
-// $('a').hover(linkIndicator);
