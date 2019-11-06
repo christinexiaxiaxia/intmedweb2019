1c1ac3b5210f84e0a8f942395eaafd993b0f2372
Christine <christine-xia@hotmail.com>
2019-11-06 12:18:54 -0500

    Trying to fix upload error by removing node modules

--- reading-response-3/js/main.js
+++ reading-response-3/js/main.js
@@ -5,15 +5,19 @@ var timeout = null;
 $(document).on('mousemove', function() {
     clearTimeout(timeout);
 
-    timeout = setTimeout(function() {
-		$('.wave').animate({
-			opacity: 1,
-			}, 15000, function() {
-		});
-		$('img').animate({
-			'filter': 'blur(100px)',
-			}, 15000, function() {
-		});
-    }, 2000);
-});
+	$('.wave').stop().animate({opacity: 0}, 1000, function() {
+		$('.wave').toggle('fast', function(){
+			$('.wave').animate({opacity: 1})
+		})
+	});
+	console.log("Mouse is moving")
 
+    timeout = setTimeout(function() {
+		$('.wave').stop().animate({opacity: 1}, 15000, function() {
+			$('.wave').toggle('fast', function (){
+				$('.wave').animate({opacity: 1});
+			})
+		});
+		console.log("Mouse is idle")
+    }, 1000);
+});
