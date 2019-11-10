fddafc9a270661fc0045806be22c5f13dfc9fbfb
Christine <christine-xia@hotmail.com>
2019-11-09 20:56:12 -0500

    Added title

--- reading-response-3/js/main.js
+++ reading-response-3/js/main.js
@@ -5,7 +5,7 @@ var timeout = null;
 $(document).on('mousemove', function() {
     clearTimeout(timeout);
 
-	$('.wave').stop().animate({opacity: 0}, 1000, function() {
+	$('.wave').stop().animate({opacity: 0}, 2000, function() { // LENGTH OF TIME FOR BLUE FADE OUT ON MOUSE MOVE
 		$('.wave').toggle('fast', function(){
 			$('.wave').animate({opacity: 1})
 		})
@@ -13,11 +13,11 @@ $(document).on('mousemove', function() {
 	console.log("Mouse is moving")
 
     timeout = setTimeout(function() {
-		$('.wave').stop().animate({opacity: 1}, 15000, function() {
+		$('.wave').stop().animate({opacity: 1}, 15000, function() { // LENGTH OF TIME FOR BLUE TO FADE IN WHEN MOUSE STILL
 			$('.wave').toggle('fast', function (){
 				$('.wave').animate({opacity: 1});
 			})
 		});
 		console.log("Mouse is idle")
-    }, 1000);
+    }, 1000); // LAG BEFORE BLUE STARTS APPEARING
 });
