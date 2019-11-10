8f36288d1ce9d993be4307f6a77a76ecb3261931
Christine <christine-xia@hotmail.com>
2019-11-09 21:04:14 -0500

    Adjusted opacity to fix blue-disappearing glitch

--- reading-response-3/js/main.js
+++ reading-response-3/js/main.js
@@ -5,7 +5,7 @@ var timeout = null;
 $(document).on('mousemove', function() {
     clearTimeout(timeout);
 
-	$('.wave').stop().animate({opacity: 0}, 2000, function() { // LENGTH OF TIME FOR BLUE FADE OUT ON MOUSE MOVE
+	$('.wave').stop().animate({opacity: 0.0001}, 2000, function() { // LENGTH OF TIME FOR BLUE FADE OUT ON MOUSE MOVE
 		$('.wave').toggle('fast', function(){
 			$('.wave').animate({opacity: 1})
 		})
