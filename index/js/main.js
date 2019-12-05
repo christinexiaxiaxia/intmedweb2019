f68f7a3873a1b174d98d23f16f80b14e6115b837
Christine <christine-xia@hotmail.com>
2019-12-05 00:43:27 -0500

    Index updates

---
+++ index/js/main.js
@@ -0,0 +1,59 @@
+$('.shelf.a').mousedown(function(){
+	$('.case').css({'grid-template-columns':'50% 1fr 1fr 1fr 1fr 1fr'});
+	$('.title.a').css({'font-family':'University','font-size':'2rem'})
+	$('.title.b').css({'font-family':'Sneaky','font-size':'75%'})
+	$('.title.c').css({'font-family':'Sneaky','font-size':'75%'})
+	$('.title.d').css({'font-family':'Sneaky','font-size':'75%'})
+	$('.title.e').css({'font-family':'Sneaky','font-size':'75%'})
+	$('.title.f').css({'font-family':'Sneaky','font-size':'75%'})
+})
+
+$('.shelf.b').mousedown(function(){
+	$('.case').css({'grid-template-columns':'1fr 50% 1fr 1fr 1fr 1fr'})
+	$('.title.a').css({'font-family':'Sneaky','font-size':'75%'})
+	$('.title.b').css({'font-family':'University','font-size':'2rem'})
+	$('.title.c').css({'font-family':'Sneaky','font-size':'75%'})
+	$('.title.d').css({'font-family':'Sneaky','font-size':'75%'})
+	$('.title.e').css({'font-family':'Sneaky','font-size':'75%'})
+	$('.title.f').css({'font-family':'Sneaky','font-size':'75%'})
+})
+
+$('.shelf.c').mousedown(function(){
+	$('.case').css({'grid-template-columns':'1fr 1fr 50% 1fr 1fr 1fr'})
+	$('.title.a').css({'font-family':'Sneaky','font-size':'75%'})
+	$('.title.b').css({'font-family':'Sneaky','font-size':'75%'})
+	$('.title.c').css({'font-family':'University','font-size':'2rem'})
+	$('.title.d').css({'font-family':'Sneaky','font-size':'75%'})
+	$('.title.e').css({'font-family':'Sneaky','font-size':'75%'})
+	$('.title.f').css({'font-family':'Sneaky','font-size':'75%'})
+})
+
+$('.shelf.d').mousedown(function(){
+	$('.case').css({'grid-template-columns':'1fr 1fr 1fr 50% 1fr 1fr'})
+	$('.title.a').css({'font-family':'Sneaky','font-size':'75%'})
+	$('.title.b').css({'font-family':'Sneaky','font-size':'75%'})
+	$('.title.c').css({'font-family':'Sneaky','font-size':'75%'})
+	$('.title.d').css({'font-family':'University','font-size':'2rem'})
+	$('.title.e').css({'font-family':'Sneaky','font-size':'75%'})
+	$('.title.f').css({'font-family':'Sneaky','font-size':'75%'})
+})
+
+$('.shelf.e').mousedown(function(){
+	$('.case').css({'grid-template-columns':'1fr 1fr 1fr 1fr 50% 1fr'})
+	$('.title.a').css({'font-family':'Sneaky','font-size':'75%'})
+	$('.title.b').css({'font-family':'Sneaky','font-size':'75%'})
+	$('.title.c').css({'font-family':'Sneaky','font-size':'75%'})
+	$('.title.d').css({'font-family':'Sneaky','font-size':'75%'})
+	$('.title.e').css({'font-family':'University','font-size':'2rem'})
+	$('.title.f').css({'font-family':'Sneaky','font-size':'75%'})
+})
+
+$('.shelf.f').mousedown(function(){
+	$('.case').css({'grid-template-columns':'1fr 1fr 1fr 1fr 1fr 50%'})
+	$('.title.a').css({'font-family':'Sneaky','font-size':'75%'})
+	$('.title.b').css({'font-family':'Sneaky','font-size':'75%'})
+	$('.title.c').css({'font-family':'Sneaky','font-size':'75%'})
+	$('.title.d').css({'font-family':'Sneaky','font-size':'75%'})
+	$('.title.e').css({'font-family':'Sneaky','font-size':'75%'})
+	$('.title.f').css({'font-family':'University','font-size':'2rem'})
+})
