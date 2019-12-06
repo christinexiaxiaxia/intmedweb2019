d7dcf5dc1f96dead5f371eff42e443abf9441bda
Christine <christine-xia@hotmail.com>
2019-12-05 21:02:21 -0500

    Index!!!

--- index/js/main.js
+++ index/js/main.js
@@ -1,24 +1,58 @@
-$('.shelf.a').mousedown(function(){
-	$('.case').css({'grid-template-columns':'4rem 50% 1fr 1fr 1fr 1fr 1fr 4rem'});
-	$('.title.a').css({'font-family':'Ribes','font-size':'4rem','line-height':'4rem','margin-top':'1rem','text-transform':'uppercase'})	
-	// $('.title.a').addClass('.title-clicked')
-	// $('.subtitle.a').css({'font-size':'4rem'})
+
+$('.desc').hide();
+
+
+// COLUMN CLICK STUFF
+
+$('.bigboy').mousedown(function(){
+	$('.case').css({'grid-template-columns':'4rem 1fr 1fr 1fr 1fr 1fr 1fr 4rem'});
+	$('.title.a').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})	
 	$('.title.b').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
 	$('.title.c').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
 	$('.title.d').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
 	$('.title.e').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
 	$('.title.f').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
+	
+	$('.desc.a').hide();
+	$('.desc.b').hide();
+	$('.desc.c').hide();
+	$('.desc.d').hide();
+	$('.desc.e').hide();
+	$('.desc.f').hide();
+})
+
+$('.shelf.a').mousedown(function(){
+	$('.case').css({'grid-template-columns':'4rem 50% 1fr 1fr 1fr 1fr 1fr 4rem'});
+	$('.title.a').css({'font-family':'Ribes','font-size':'4rem','line-height':'4rem','margin-top':'1rem','text-transform':'uppercase'})	
+	$('.title.b').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
+	$('.title.c').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
+	$('.title.d').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
+	$('.title.e').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
+	$('.title.f').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
+	
+	$('.desc.a').show();
+	$('.desc.b').hide();
+	$('.desc.c').hide();
+	$('.desc.d').hide();
+	$('.desc.e').hide();
+	$('.desc.f').hide();
 })
 
 $('.shelf.b').mousedown(function(){
 	$('.case').css({'grid-template-columns':'4rem 1fr 50% 1fr 1fr 1fr 1fr 4rem'})
 	$('.title.a').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
 	$('.title.b').css({'font-family':'Ribes','font-size':'4rem','line-height':'4rem','margin-top':'1rem','text-transform':'uppercase'})
-	// $('.subtitle.b').css({'font-size':'4rem'})
 	$('.title.c').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
 	$('.title.d').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
 	$('.title.e').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
 	$('.title.f').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
+	
+	$('.desc.a').hide();
+	$('.desc.b').show();
+	$('.desc.c').hide();
+	$('.desc.d').hide();
+	$('.desc.e').hide();
+	$('.desc.f').hide();
 })
 
 $('.shelf.c').mousedown(function(){
@@ -26,10 +60,16 @@ $('.shelf.c').mousedown(function(){
 	$('.title.a').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
 	$('.title.b').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
 	$('.title.c').css({'font-family':'Ribes','font-size':'4rem','line-height':'4rem','margin-top':'1rem','text-transform':'uppercase'})
-	// $('.subtitle.c').css({'font-size':'4rem'})
 	$('.title.d').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
 	$('.title.e').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
 	$('.title.f').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
+	
+	$('.desc.a').hide();
+	$('.desc.b').hide();
+	$('.desc.c').show();
+	$('.desc.d').hide();
+	$('.desc.e').hide();
+	$('.desc.f').hide();
 })
 
 $('.shelf.d').mousedown(function(){
@@ -38,9 +78,15 @@ $('.shelf.d').mousedown(function(){
 	$('.title.b').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
 	$('.title.c').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
 	$('.title.d').css({'font-family':'Ribes','font-size':'4rem','line-height':'4rem','margin-top':'1rem','text-transform':'uppercase'})
-	// $('.subtitle.d').css({'font-size':'4rem'})
 	$('.title.e').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
 	$('.title.f').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
+	
+	$('.desc.a').hide();
+	$('.desc.b').hide();
+	$('.desc.c').hide();
+	$('.desc.d').show();
+	$('.desc.e').hide();
+	$('.desc.f').hide();
 })
 
 $('.shelf.e').mousedown(function(){
@@ -50,8 +96,14 @@ $('.shelf.e').mousedown(function(){
 	$('.title.c').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
 	$('.title.d').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
 	$('.title.e').css({'font-family':'Ribes','font-size':'4rem','line-height':'4rem','margin-top':'1rem','text-transform':'uppercase'})
-	// $('.subtitle.e').css({'font-size':'4rem'})
 	$('.title.f').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
+	
+	$('.desc.a').hide();
+	$('.desc.b').hide();
+	$('.desc.c').hide();
+	$('.desc.d').hide();
+	$('.desc.e').show();
+	$('.desc.f').hide();
 })
 
 $('.shelf.f').mousedown(function(){
@@ -62,5 +114,11 @@ $('.shelf.f').mousedown(function(){
 	$('.title.d').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
 	$('.title.e').css({'font-family':'Basis','font-size':'1rem','line-height':'1rem','margin-top':'0rem','text-transform':'capitalize'})
 	$('.title.f').css({'font-family':'Ribes','font-size':'4rem','line-height':'4rem','margin-top':'1rem','text-transform':'uppercase'})
-	// $('.subtitle.f').css({'font-size':'4rem'})
+	
+	$('.desc.a').hide();
+	$('.desc.b').hide();
+	$('.desc.c').hide();
+	$('.desc.d').hide();
+	$('.desc.e').hide();
+	$('.desc.f').show();
 })
