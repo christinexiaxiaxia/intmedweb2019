fb9f3c961dd2cdd41925b2e543f5bf45a8d23bc8
Christine <christine-xia@hotmail.com>
2020-02-16 00:38:34 -0500

    Removed more cursed files

--- act-3/tests/infinite-scroll/script.js
+++
@@ -1,6 +0,0 @@
-$('.article-feed').infiniteScroll({
-  path: '.pagination__next',
-  append: '.article',
-  status: '.scroller-status',
-  hideNav: '.pagination',
-});
--- act-3/tests/site/js/main.js
+++
@@ -1,8 +0,0 @@
-// INFINITE SCROLL https://infinite-scroll.com/
-
-$('.wrapper').infiniteScroll({
-  // options
-  path: '.pagination__next',
-  append: '.post',
-  history: false,
-});
